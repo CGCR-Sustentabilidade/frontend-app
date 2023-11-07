9c334ec7db6e510e7695a92cae714a1389a48cc5
Fernanda Torres <fernanda.sttorres@gmail.com>
2023-11-07 08:47:07 -0300

    feat: create the details card

---
+++ src/components/DetailsCard/DetailsCard.styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+    width: 1110px;
+    height: 85px;
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto;
+    align-items: center;
+    justify-content: space-around;
+    background-color: #fff;
+`;
+
+export const IconsDiv = styled.div`
+    display: grid;
+    grid-template-columns: auto auto ;
+    gap: 30px;
+    align-items: center;
+`;
+
+export const Image = styled.img`
+    width: 24px;
+`;
+
+export const Icons = styled.img`
+    width: 16px;
+`;

5742ea60f00f0189a680982c8c0fa78c64e1bc35
Fernanda Torres <fernanda.sttorres@gmail.com>
2023-11-07 12:31:58 -0300

    feat: create the edit and delete functions

--- src/components/DetailsCard/DetailsCard.styled.js
+++ src/components/DetailsCard/DetailsCard.styled.js
@@ -1,26 +1,27 @@
 import styled from "styled-components";
 
-export const Card = styled.div`
-    width: 1110px;
-    height: 85px;
-    display: grid;
-    grid-template-columns: auto auto auto auto auto auto auto;
-    align-items: center;
-    justify-content: space-around;
-    background-color: #fff;
+export const DetailsCardStyled = styled.div`
+  width: 1110px;
+  height: 85px;
+  display: grid;
+  grid-template-columns: auto auto auto auto auto auto auto;
+  align-items: center;
+  justify-content: space-around;
+  background-color: #fff;
 `;
 
 export const IconsDiv = styled.div`
-    display: grid;
-    grid-template-columns: auto auto ;
-    gap: 30px;
-    align-items: center;
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 30px;
+  align-items: center;
 `;
 
 export const Image = styled.img`
-    width: 24px;
+  width: 24px;
 `;
 
 export const Icons = styled.img`
-    width: 16px;
+  width: 16px;
+  cursor: pointer;
 `;

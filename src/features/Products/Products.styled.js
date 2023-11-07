64a3edf06f560076ce6ca13b17a060ff3a18104f
Fernanda Torres <fernanda.sttorres@gmail.com>
2023-11-07 18:21:45 -0300

    feat: edit products screen

--- src/features/Products/Products.styled.js
+++ src/features/Products/Products.styled.js
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
-export const ProductConteiner = styled.div`
-    margin: 0 px;
-    padding: 30px;
-    width: 100%;
-    background-color: #f8f8f8;
+export const ProductsList = styled.div`
+  margin: 0;
+  padding: 0px;
+  width: 100%;
+  background-color: #f8f8f8;
+  display: flex;
+  align-items: flex-start;
+`;
+
+export const ProductsConteiner = styled.div`
+  width: 1070px;
+  margin: 0px 30px 0 30px;
+  display: grid;
 `;
